b631b8ada731fdbdaa0c2fc531edecb1015bdbf4
Patrick Lamoureux <patmacmini@Patricks-Mini.domain>
2020-03-24 21:35:28 -0600

    put app js right and updated pokecards

--- src/componennts/Pokecards.js
+++ src/componennts/Pokecards.js
@@ -21,10 +21,41 @@ const Pokecards = () => {
     })
   },[])
 
+  const mapImage = cards.map(singleMon=>{
+    if (singleMon.id === 0) {
+      singleMon.imageID = 0;
+    }
+    if (singleMon.id < 10) {
+      singleMon.imageID = "00" + singleMon.id;
+    }
+    if (singleMon.id > 9 && singleMon.id < 100) {
+      singleMon.imageID = "0" + singleMon.id;
+    }
+    if (singleMon.id > 99) {
+      singleMon.imageID = singleMon.id;
+    }
+  
+    if (singleMon.name === "mr-mime") {
+      singleMon.name = "mr_Mime";
+    }
+    if (singleMon.name === "mime-jr") {
+      singleMon.name = "mime_jr";
+    }
+  
+    let dashIndex = singleMon.name.indexOf("-");
+    if (dashIndex !== -1) {
+      // Found a few pokemon who have a dash in their name
+      singleMon.name = singleMon.name.slice(0, dashIndex);
+    }
+    singleMon.name =
+      singleMon.name.charAt(0).toUpperCase() + singleMon.name.slice(1);
+    return singleMon;
+  })
+
   const renderItems = () => {
     if(!error){
-      return cards.map((p, idx)=>(
-        <Pokecard key={idx} singleMon={p} />
+      return mapImage.map((p, idx)=>(
+        <Pokecard key={idx} singleMon={p} image={`${p.imageID}`} />
       ))
     } else{
       return <Error />
